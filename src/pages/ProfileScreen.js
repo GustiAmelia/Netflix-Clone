6482099867f365df05b633cbe4978c24832f32f2
AmeliaGusti <gustiamelia@tebengan.id>
2022-03-28 14:30:06 +0700

    profile screens

--- src/pages/ProfileScreen.js
+++ src/pages/ProfileScreen.js
@@ -21,7 +21,14 @@ function ProfileScreen() {
           <div className="profileScreen__details">
             <h2>{user.email}</h2>
             <div className="profileScreen__plans">
-              <h3>Plans</h3>
+              <h3>Plans (Current Plan: premium)</h3>
+              <h4>date: 29/03/2021</h4>
+              <div className="profileScreen__optionPlans">
+                <div className="profileScreen__optionPlan">
+                  <h4>Netflix Standard</h4>
+                  <h5></h5>
+                </div>
+              </div>
               <button
                 onClick={() => auth.signOut()}
                 className="profileScreen__signOut">Sign Out</button>
